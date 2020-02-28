a54433f266734cc85ac2c6b313957819ffff9dc9
leon-do <leondo123@gmail.com>
2020-02-28 13:11:27 -0500

    build

---
+++ build/precache-manifest.2e7e5d1bf09dda769c60e0e4a0bdf654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7704b547d68839bdb8d07a3a165cefe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f91e269ac31db2217060",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dbbd3e92ad6141b80015",
+    "url": "/static/js/2.5d59c51d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5d59c51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91e269ac31db2217060",
+    "url": "/static/js/main.dfa4b671.chunk.js"
+  },
+  {
+    "revision": "aaed884b9cd5f8f87494",
+    "url": "/static/js/runtime-main.97472ead.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
